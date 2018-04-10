69268d474fe62360e261fb3acaf4bcc1d093b706
yellowsman <sigesigeito@gmail.com>
2018-04-10 23:52:36 +0900

    rename function

--- lib/ipo_schedule.ex
+++ lib/ipo_schedule.ex
@@ -19,25 +19,29 @@ defmodule IPO_SCHEDULE do
   def run do
     client
     fetch(client)
+    fetch_calendar
     scrape_ipo_schedule
     compare_schedule(scrape_schedule, calendar_schedule)
-    update_schedule
+    update_calendar
   end
 
   # Google Calendar Api v3
   def :client do
   end
 
-  def :fetch_schedule(token) do
+  def :fetch_calendar(token) do
   end
 
-  def :update_schedule(token, schedule) do
+  def :update_calendar(token, schedule) do
   end
 
   def compare_schedule(scrape_schedule, calendar_schedule) do
   end
 
   # http://www.ipokiso.com/company/schedule.html
+  def fetch_ipo_schedule do
+  end
+  
   def scrape_ipo_schedule do
   end
 end
